185873b0e8bf8f4c6f73ea8e1ad1258becb49b16
Jason Jones <jason.e.jones@gmail.com>
2021-04-10 08:10:23 -0400

    worked out the server.js mostly

--- server.js
+++ server.js
@@ -1,26 +1,41 @@
+// Uncaught SyntaxError: Unexpected token '<'
 //server.js
 const express = require('express'),
-      server = express();
+      path = require('path'),
+      app = express(),
+      PORT = 3000;
 
-server.set('port', process.env.PORT || 3000);
+// set server port to either the value defined in PORT, or if that's not set, then 3000
+app.set('port', process.env.PORT || 3000);
+app.use("/assets", express.static('./assets'));
+
+// Sets up the Express app to handle data parsing
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
 
 //Basic routes
-server.get('/', (request,response)=>{
-   response.send('Home page');
+app.get('/notes', (req,res) => {
+   res.sendFile(path.join(__dirname, '/notes.html'));
+   console.log(path.join(__dirname, './notes.html'));
 });
 
-server.get('/about',(request,response)=>{
-   response.send('About page');
+app.get('/index', (req,res) => { 
+   res.sendFile(path.join(__dirname, './index.html'));
+   console.log(path.join(__dirname, './index.html'));
 });
 
-//Express error handling middleware
-server.use((request,response)=>{
-   response.type('text/plain');
-   response.status(505);
-   response.send('Error page');
-});
+
+
+
+// Express error handling middleware
+// with a '*' route - we shouldn't be hitting this often
+// server.use((request,response)=>{
+//    response.type('text/plain');
+//    response.status(505);
+//    response.send('Error page');
+// });
 
 //Binding to a port
-server.listen(3000, ()=>{
+app.listen(3000, ()=>{
   console.log('Express server started at port 3000');
 });

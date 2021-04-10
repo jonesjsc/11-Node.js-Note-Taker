986cc527275bfb11809a4bb2553f9d582f3a00ec
Jason Jones <jason.e.jones@gmail.com>
2021-04-10 10:08:20 -0400

    10am push

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -10,6 +10,7 @@ if (window.location.pathname === '/notes') {
   saveNoteBtn = document.querySelector('.save-note');
   newNoteBtn = document.querySelector('.new-note');
   noteList = document.querySelectorAll('.list-container .list-group');
+  console.log("we are working a notes page");
 }
 
 // Show an element
--- server.js
+++ server.js
@@ -2,40 +2,41 @@
 //server.js
 const express = require('express'),
       path = require('path'),
+      fs = require('fs'),
       app = express(),
       PORT = 3000;
 
 // set server port to either the value defined in PORT, or if that's not set, then 3000
-app.set('port', process.env.PORT || 3000);
+// app.set('port', process.env.PORT || 3000);
+
 app.use("/assets", express.static('./assets'));
+app.use("/db", express.static('./db'));
+
 
 // Sets up the Express app to handle data parsing
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-//Basic routes
+// Basic routes
 app.get('/notes', (req,res) => {
    res.sendFile(path.join(__dirname, '/notes.html'));
-   console.log(path.join(__dirname, './notes.html'));
 });
 
-app.get('/index', (req,res) => { 
-   res.sendFile(path.join(__dirname, './index.html'));
-   console.log(path.join(__dirname, './index.html'));
+app.get('/api/nodes', (req,res) => {
+console.log ("api dump here bois");
 });
 
-
-
+app.get('*', (req,res) => { 
+   res.sendFile(path.join(__dirname, '/index.html'));
+});
 
 // Express error handling middleware
 // with a '*' route - we shouldn't be hitting this often
-// server.use((request,response)=>{
-//    response.type('text/plain');
-//    response.status(505);
-//    response.send('Error page');
-// });
+app.use((req,res)=>{
+   res.type('text/plain');
+   res.status(505);
+   res.send('Error page');
+});
 
 //Binding to a port
-app.listen(3000, ()=>{
-  console.log('Express server started at port 3000');
-});
+app.listen(PORT, ()=> console.log(`Express server started at port ${PORT}`));

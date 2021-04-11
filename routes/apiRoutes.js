42776efb1e3513307fdb7a5e9362547984e452b6
Jason Jones <jason.e.jones@gmail.com>
2021-04-10 22:22:57 -0400

    app.get finally reads the file and send a response

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,17 @@
-// LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
-
+// our Notes Database is really just a json file
 const notesData = require('../db/db.json');
-// const waitListData = require('../data/waitinglistData');
+const notesDataFile = './db/db.json';
+
+// console.log("CL "+notesData);
+// console.log("Notes Data File "+notesDataFile);
+
+const util = require('util');
+const fs = require('fs');
+
+// import { v1 as uuidv1 } from 'uuid'; // Used to generate the Unique ID stored with the note
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
 
 // ROUTING
 
@@ -11,35 +19,53 @@ module.exports = (app) => {
   // API GET Requests
   // Below code handles when users "visit" a page.
   // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // (ex: localhost:PORT/api/notes... they are shown a JSON of the data in the table)
   // ---------------------------------------------------------------------------
 
-  app.get('/api/notes', (req, res) => res.json(notesData));
+  // app.get('/api/notes', (req, res) => res.json(notesData));
+  app.get('/api/notes', (req, res) => {
+    readFileAsync('./db/db.json', 'utf8')
+    // If promise resolved and datas are read 
+    .then(data => {
+      const contents = data.toString()
+      console.log(`\nContents of the file :\n${contents}`)
+      res.send(contents);
+    })
+  
+    // If promise get rejected
+    .catch(err => {
+      console.log(`Error occurs, Error code -> ${err.code}, 
+      Error No -> ${err.errno}`);
+    });
+  });
 
-  // app.get('/api/waitlist', (req, res) => res.json(waitListData));
-
-  // API POST Requests
+    // API POST Requests
   // Below code handles when a user submits a form and thus submits data to the server.
   // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
+  // ...the JSON is pushed to the JSON file
   // ---------------------------------------------------------------------------
 
   app.post('/api/notes', (req, res) => {
     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
     // It will do this by sending out the value "true" have a table
     // req.body is available since we're using the body parsing middleware
-    if (notesData.length < 5) {
-      notesData.push(req.body);
-      res.json(true);
-    } 
+   
+    notesData.push(req.body);
+    res.json(true);
+
+    });
+
+    ///-----
+    // if (notesData.length < 5) {
+    //   notesData.push(req.body);
+    //   res.json(true);
+    // } 
     // else {
     //   wai
     //   tListData.push(req.body);
     //   res.json(false);
     // }
-  });
+  // });
 
   // I added this below code so you could clear out the table while working with the functionality.
   // Don"t worry about it!
